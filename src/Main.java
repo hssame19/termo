c01c1f8047a51510bad14663d9ef665fe2339f36
Henrique <hssame19@gmail.com>
2026-01-07 23:18:22 -0300

    Ajustes na lógica de letras duplicadas

--- src/Main.java
+++ src/Main.java
@@ -1,69 +1,80 @@
 public void main(String[] args) {
 
-    ArrayList<Character> tbg = new ArrayList<Character>();
-    ArrayList<Character> tbgComp = new ArrayList<Character>();
-
     boolean win = false;
-
-    int attempts = 0;
-
-    String answer = "STRING";
 
     Scanner scanner = new Scanner(System.in);
 
-    System.out.println(".: Termo (6 letras) :.\nSe a letra estiver no lugar correto, ela aparecerá na palavra.\nSe a letra estiver na palavra mas no lugar errado, aparecerá uma interrogação.\nE se a letra não estiver na palavra, aparecerá um asterísco.\n");
-    do {
+    int attempts = 0;
+    String answer = "STRING";
+    String guess = "";
 
-        for (int i = 0; i < answer.length(); i++) {
+    System.out.println(".: Termo :. (6 letras)\n");
+
+    do {
+        ArrayList<Character> tbg = new ArrayList<Character>();
+
+        for(int i = 0; i < answer.length(); i++) {
             tbg.add(answer.charAt(i));
         }
 
-        for (int i = 0; i < answer.length(); i++) {
-            tbgComp.add(answer.charAt(i));
+        try {
+            guess = scanner.nextLine();
+        } catch(InputMismatchException e) {
+            System.out.println("Digite uma palavra.\n");
         }
 
-        String guess = scanner.nextLine();
         guess = guess.toUpperCase();
 
-        for (int i = 0; i < answer.length(); i++) {
-            if (answer.contains(guess.substring(i, i + 1))) {
-                if (tbg.contains(guess.charAt(i))) {
-                    if (guess.charAt(i) == (tbgComp.get(i))) {
-                        System.out.print(guess.charAt(i));
+        if(guess.length() != 6) {
+            System.out.println("A palavra precisa ter 6 letras!\n");
+            continue;
+        }
 
-                        char temp = guess.charAt(i);
+        for(int i = 0; i < guess.length(); i++) {
+            if(guess.charAt(i) == tbg.get(i)) {
+                tbg.set(i, '/');
+            }
+        }
 
-                        for (int j = 0; j < tbg.size(); j++) {
-                            if (tbg.get(j).equals(temp)) {
-                                tbg.remove(j);
-                            }
+        for(int i = 0; i < guess.length(); i++) {
+            if(answer.contains(guess.substring(i, i + 1))) {
+                if(!tbg.contains(guess.charAt(i)) && guess.charAt(i) == answer.charAt(i)) {
+                    System.out.print(guess.charAt(i));
+                } else if(tbg.contains(guess.charAt(i)) && guess.charAt(i) != answer.charAt(i)) {
+                    System.out.print("?");
+
+                    for(int j = 0; j < tbg.size(); j++) {
+                        if(tbg.get(j) == guess.charAt(i)) {
+                            tbg.set(j, '/');
+                            break;
                         }
-
-                    } else {
-                        System.out.print("?");
                     }
-                } else {
+
+                } else if(!tbg.contains(guess.charAt(i))) {
                     System.out.print("*");
                 }
             } else {
                 System.out.print("*");
             }
         }
-        System.out.println("\n");
 
         attempts++;
 
         if(guess.equals(answer)) {
             win = true;
         }
+
+        System.out.println("\n");
+
     } while(!win);
 
-    if(attempts == 1) {
+    if (attempts == 1) {
         System.out.println("De primeira!");
-    } else if(attempts > 1 && attempts <= 3) {
+    } else if (attempts > 1 && attempts <= 3) {
         System.out.println("Impressionante!");
     } else if (attempts > 3) {
         System.out.println("Bom trabalho!");
     }
+
     System.out.print("A resposta era: " + answer);
 }
